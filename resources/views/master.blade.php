67726bfaab323a1906b19b81a359e1e723eb5e01
ericgbekor <ericgbekor@gmail.com>
2017-03-14 09:13:35 +0000

    master page updated

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,15 +13,15 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <meta name="keywords" content="Seeking Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<link href="{{URL::asset('css/bootstrap-3.1.1.min.css')}}" rel='stylesheet' type='text/css' />
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+<script src="{{URL::asset('js/jquery.min.js')}}"></script>
+<script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
 <!-- Custom Theme files -->
-<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href="{{URL::asset('css/style.css')" rel='stylesheet' type='text/css' />
 <link href='//fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
 <!----font-Awesome----->
-<link href="css/font-awesome.css" rel="stylesheet"> 
+<link href="{{URL::asset('css/font-awesome.css')}}" rel="stylesheet"> 
 <!----font-Awesome----->
 </head>
 <body>
